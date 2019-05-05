ec8fd7dc7e0a09d3de3747949078df882c46d434
Gabriel <cgvilasnovas@gmail.com>
2019-05-05 18:03:55 -0300

    Swap Funciona Para qualquer Posicao do vetor

--- swap.asm
+++ swap.asm
@@ -1,32 +1,58 @@
 //Exemplo swap
 
 .data
-	vetor: .word 1, 2
+	vetor: .word 1, 2, 3
+	newline: .asciiz"\n"
 
 .text
+	//Macro para printar int
+	.macro print_int(%REGISTRADOR)
+		ADDI X8, XZR, 1
+		ADD X7, XZR, %REGISTRADOR
+		SVC 0
+	.end_macro
+	
+	.macro	newline
+		LDA X7, newline
+		ADDI X8, XZR, 4
+		SVC 0
+	.end_macro
 	
 	LDA X9, vetor 
+	ADDI X15, XZR, 1
 	BL SWAP
 	
 	LDUR X10, [X9, 0] //X10 = X9[0]
 	LDUR X11, [X9, 4] //X11 = X9[1]
+	LDUR X12, [X9, 8] //X12 = X9[2]  
 	
-	ADDI X8, XZR, 1
-	ADD X7, XZR, X10
-	SVC 0
+	//Print X9[0]
+	print_int(X10)
+	newline
 	
-	ADD X7, XZR, X11
-	SVC 0
+	//Print X9[1]
+	print_int(X11)
+	newline
 	
+	//Print X9[2]
+	print_int(X12)
+	newline
+	
+	//Exit
 	ADDI X8, XZR, 10
 	SVC 0
 	
-SWAP: //RECEBE O VETOR X9, E inverte o primeiro com o segundo elemento
+SWAP: //RECEBE O VETOR em X9 e K em X15 , E inverte K elemento com K+1
 
-	LDUR X0, [X9, 0] // X0 = TEMP0
-	LDUR X1, [X9, 4] // X1 = TEMP1
-	STUR X0, [X9, 4] // X9[1] = TEMP0(X9[0])
-	STUR X1, [X9, 0] // X9[0] = TEMP1(X9[1])
+	LSL X10, X15, 2 //k * 4
+	ADD X10, X9, X10//X10 = X9[k]
+	
+	LDUR X0, [X10, 0] // X0 = k
+	LDUR X1, [X10, 4] // X1 = k+1
+	
+	STUR X0, [X10, 4] // X10[k+1] = TEMP0(X10[k])
+	STUR X1, [X10, 0] // X10[k] = TEMP1(X10[K+1])
+	
 	BR X30
 	
 	
